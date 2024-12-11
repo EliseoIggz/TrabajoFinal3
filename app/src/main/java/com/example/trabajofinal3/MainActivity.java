67965bf87b181e7bc89846726a97caf393d812eb
EliseoIggz <eliseoiggz@gmail.com>
2024-12-11 16:48:33 +0100

    fragment colocado

--- app/src/main/java/com/example/trabajofinal3/MainActivity.java
+++ app/src/main/java/com/example/trabajofinal3/MainActivity.java
@@ -39,8 +39,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
+        
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
